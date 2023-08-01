601ce66184a23721062dc7228296bf3d6082079f
mw1163528384 <mw1163528384@gmail.com>
2023-08-01 21:09:01 +0800

    solve conflict

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,36 +1,82 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from "react-router-dom";
 import '../styles/HomePage.css';
-import menu_logo from '../assets/images/menu-logo.png'
+import { Calendar, momentLocalizer } from 'react-big-calendar'
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import sorry_img from '../assets/images/sorry-img.png';
+import add_ring from '../assets/images/add_ring_fill.png';
 
 const HomePage = (getUserItineraryFromServer) => {
     const navigate = useNavigate()
     const [userItinerary, setuserItinerary] = useState(null);
+    const [events, setEvents] = useState([]);
     
     useEffect(() => {
         fetchUserItinerary();
     }, [])
 
     const fetchUserItinerary = async () => {
-        const itinerary = await getUserItineraryFromServer();
-        setuserItinerary(itinerary);
+        try {
+            const response = await fetch('/api/userItinerary');
+            if (!response.ok) {
+                throw new Error("Failed to fetch user itinerary");
+            }
+
+            const data = await response.json();
+            setuserItinerary(data);
+        } catch(error) {
+            console.error('Error fetching user itinerary:', error);
+        }
     }
 
+    useEffect(() => {
+        if (userItinerary) {
+            const formattedEvents = userItinerary.map((item) => ({
+                title: item.title,
+                start: new Date(item.startDate),
+                end: new Date(item.endDate),
+            }));
+            setEvents(formattedEvents);
+        }
+    }, [userItinerary])
+    
     const handleAddTripClick = async() => {
-        navigate('/');
+        navigate('/newTrip');
     }
+
+    const localizer = momentLocalizer(moment)
 
     return (
         <div>
             {userItinerary ? (
-            <div className='body'>
+                <div className='homepage-body'>
+                    <Calendar 
+                    localizer={localizer}
+                    events={events}
+                    startAccessor="start"
+                    endAccessor="end"
+                    views={['week']}
+                    style={{height:500}} 
+                />
 
-                <div className='put your generated itinerary here!'> </div>
-
-            </div>
+                </div>
             ) : (
-            <div className='body'>
-                <button onClick={handleAddTripClick} id="addTrip-btn" className='addTrip-btn'> Click here to add new trip </button>
+            <div className='homepage-body-new'>
+                <div className='sorry_img_container'>
+                    <img src={sorry_img} alt='sorry_img'/>
+                </div>
+                
+                <div className='sorry_msg_container'>
+                    <h3>Oops! Looks like you don't have any itineraries at the moment.</h3>
+                </div>
+                
+                <div className='addTrip_btn_container'>
+                    <button onClick={handleAddTripClick} id="addTrip-btn" className='addTrip-btn'> 
+                        <img src={add_ring} alt='add_ring'/>
+                        Click here to add new trip 
+                    </button>
+                </div>
             </div>
             )}
         </div>
