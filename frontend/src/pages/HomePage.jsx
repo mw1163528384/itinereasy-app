72883dd3acb1c9934916d0f78eec56dad96421e2
mw1163528384 <mw1163528384@gmail.com>
2023-08-02 23:57:50 +0800

    edit error

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,6 @@
 // HomePage.js
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useNavigate, useLocation } from "react-router-dom";
 import '../styles/HomePage.css';
 import { Calendar, momentLocalizer } from 'react-big-calendar'
 import moment from 'moment';
