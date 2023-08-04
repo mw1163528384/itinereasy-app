a6b04cfecdcb503b82bfd8d3af101e1e00c827e5
Soh Ying Yu <sohyingyu19@gmail.com>
2023-08-05 01:38:47 +0800

    slight edits to the setting and menu logos

--- frontend/src/components/HomePageHeader.jsx
+++ frontend/src/components/HomePageHeader.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import '../styles/HomePageHeader.css';
 import menu_logo from '../assets/images/menu-logo.png';
 import { Menubar } from './MenuBar';
-import setting_logo from '../assets/images/setting-logo.png';
+import setting_logo from '../assets/images/setting-logo-btn.png';
 import { OptionMenu } from './OptionMenu';
 import { SettingMenu } from './SettingMenu';
 
@@ -40,7 +40,7 @@ function HomePageHeader({generatedItinerary}) {
         </div>
       ) : (
         <div className='homepage-header-container'>
-          <button onClick={toggleMenubar}>
+          <button className="menu-logo-btn" onClick={toggleMenubar}>
               <img src={menu_logo} alt='menu-logo' />
           </button>
 
@@ -53,7 +53,7 @@ function HomePageHeader({generatedItinerary}) {
           <h1>Itinerary</h1>
           )}
           
-          <button onClick={toggleOptionMenu}>
+          <button className="setting-logo-btn" onClick={toggleOptionMenu}>
               <img src={setting_logo} alt='setting-logo' />
           </button>
         </div>
