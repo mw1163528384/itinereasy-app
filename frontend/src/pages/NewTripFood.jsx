75dbb64dd9441b3854bf45de15f0ff3d8fd2dd30
mw1163528384 <134375513+mw1163528384@users.noreply.github.com>
2023-08-06 01:01:23 +0800

    edit input effect

--- frontend/src/pages/NewTripFood.jsx
+++ frontend/src/pages/NewTripFood.jsx
@@ -5,71 +5,90 @@ import { NewTripFooter } from '../components/NewTripFooter';
 import closingIcon from '../assets/images/close-icon.png';
 
 const NewTripFood = () => {
-    const navigate = useNavigate();
-    const [selectedButtons, setSelectedButtons] = useState([]);
-    const [foodPreferences, setFoodPreferences] = useState('');
+  const navigate = useNavigate();
+  const [selectedButtons, setSelectedButtons] = useState([]);
+  const [foodList, setFoodList] = useState([]);
+  const [currentFood, setCurrentFood] = useState('');
 
-    const handleClick = (button) => {
-      if (selectedButtons.includes(button)) {
-        setSelectedButtons(selectedButtons.filter(b => b !== button));
-      } else {
-        setSelectedButtons([...selectedButtons, button]);
+  const handleKeyDown = (event) => {
+    if (event.key === 'Enter') {
+      if (currentFood.trim() !== '') {
+        setFoodList((prevFoodList) => [...prevFoodList, currentFood]);
+        setCurrentFood(''); // Clear the input after adding the food
       }
+    }
+  };
+  
+  const handleClick = (button) => {
+    if (selectedButtons.includes(button)) {
+      setSelectedButtons(selectedButtons.filter(b => b !== button));
+    } else {
+      setSelectedButtons([...selectedButtons, button]);
+    }
+  };
+
+  const handleFoodPreferencesChange = (event) => {
+    setCurrentFood(event.target.value);
+  };
+
+  const handleBack = () => {
+    navigate('/tripActivities');
+  };
+
+  const handleNext = () => {
+    const userPreferences = {
+      foodType: selectedButtons,
+      specificFoodPreferences: foodList,
     };
+    navigate('/tripGenerate', { state: { userPreferences } });
+  };
 
-    const handleFoodPreferencesChange = (event) => {
-      setFoodPreferences(event.target.value);
-    };
+  const buttons = ["Local", "Korean", "Chinese", "Burgers", "Spam musubi", "Japanese", "Shaved ice"];
 
-    const handleBack = () => {
-      navigate('/tripActivities');
-    };
+  return (
+    <div>
+      <div className='body'>
+        <div className='body-food'>
+        <img 
+            src={closingIcon} 
+            alt="Close" 
+            className="closing-btn" 
+            onClick={() => navigate("/homePage")} 
+        />
+          <h2>Food</h2>
+          <h5>Food preferences</h5>
+          <p>Add your favourite food preferences below, or specific dishes you want to try abroad.</p>
 
-    const handleNext = () => {
-      const userPreferences = {
-        foodType: selectedButtons,
-        specificFoodPreferences: foodPreferences,
-      };
-      navigate('/tripGenerate', { state: { userPreferences } });
-    };
-
-    const buttons = ["Local", "Korean", "Chinese", "Burgers", "Spam musubi", "Japanese", "Shaved ice"];
-
-    return (
-      <div>
-        <div className='body'>
-          <div className='body-food'>
-          <img 
-              src={closingIcon} 
-              alt="Close" 
-              className="closing-btn" 
-              onClick={() => navigate("/homePage")} 
-          />
-            <h2>Food</h2>
-            <h5>Food preferences</h5>
-            <p>Add your favourite food preferences below, or specific dishes you want to try abroad.</p>
-
-            <div className='food-type-selection'>
-              {buttons.map(button => (
-                <button 
-                  key={button}
-                  onClick={() => handleClick(button)}
-                  className={selectedButtons.includes(button) ? 'selected' : ''} // Add 'selected' class if the button is selected
-                >
-                  {button}
-                </button>
-              ))}
-            </div>
-
-            <label className='add-food'>
-              <input type='text' value={foodPreferences} onChange={handleFoodPreferencesChange} placeholder='Type here to add food' />
-            </label>
-
-            <NewTripFooter handleBack={handleBack} handleNext={handleNext} isFoodPage={true} />   
+          <div className='food-type-selection'>
+            {buttons.map(button => (
+              <button 
+                key={button}
+                onClick={() => handleClick(button)}
+                className={selectedButtons.includes(button) ? 'selected' : ''} // Add 'selected' class if the button is selected
+              >
+                {button}
+              </button>
+            ))}
           </div>
+
+          <div className='new-food-box'>
+          {foodList.map((foodItem, index) => (
+            <div key={index} className='new-food-content'>
+            {foodItem}
+            </div>
+          ))}
+          </div>
+
+          <label className='add-food'>
+            <input type='text' value={currentFood} onKeyDown={handleKeyDown} onChange={handleFoodPreferencesChange} placeholder='Type here to add food' />
+          </label>
+
+          <NewTripFooter handleBack={handleBack} handleNext={handleNext} isFoodPage={true} />   
         </div>
       </div>
-    );
+    </div>
+  );
 }
 
 export { NewTripFood };
+
